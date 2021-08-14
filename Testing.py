c938d295df6a18ecaf590bb82da830ac3ab9f8d0
Prabhat-codes <prabhat_namdharani@yahoo.com>
2021-08-14 21:57:30 +0530

    Order By Clause

--- Testing.py
+++ Testing.py
@@ -12,10 +12,26 @@ sql ='''DROP TABLE IF EXISTS EMPLOYEE;
             AGE INT,
             SEX CHAR(1),
             INCOME FLOAT
-            )
-        
+            )    
      '''
 cursor.execute(sql)
 print("Table created successfully........")
+
+# Preparing SQL queries to INSERT a record into the database.
+cursor.execute('''INSERT INTO EMPLOYEE(FIRST_NAME, LAST_NAME, AGE, SEX, INCOME) VALUES ('Ramya', 'Rama priya', 27, 'F', 9000)''')
+cursor.execute('''INSERT INTO EMPLOYEE(FIRST_NAME, LAST_NAME, AGE, SEX, INCOME) VALUES ('Vinay', 'Battacharya', 20, 'M', 6000)''')
+cursor.execute('''INSERT INTO EMPLOYEE(FIRST_NAME, LAST_NAME, AGE, SEX, INCOME) VALUES ('Sharukh', 'Sheik', 25, 'M', 8300)''')
+cursor.execute('''INSERT INTO EMPLOYEE(FIRST_NAME, LAST_NAME, AGE, SEX, INCOME) VALUES ('Sarmista', 'Sharma', 26, 'F', 10000)''')
+cursor.execute('''INSERT INTO EMPLOYEE(FIRST_NAME, LAST_NAME, AGE, SEX, INCOME) VALUES ('Tripthi', 'Mishra', 24, 'F', 6000)''')
+
+cursor.execute('''SELECT * from EMPLOYEE''')
+# #Fetching 1st row from the table
+# result = cursor.fetchone()
+# print(result)
+
+#Fetching 1st row from the table
+result = cursor.fetchall();
+print(result)
+
 #Closing the connection
 conn.close()
--- orderBy_clause.py
+++ orderBy_clause.py
@@ -6,21 +6,23 @@ host='127.0.0.1', port= '5432')
 conn.autocommit = True
 #Creating a cursor object using the cursor() method
 cursor = conn.cursor()
+
 #Doping EMPLOYEE table if already exists.
 cursor.execute("DROP TABLE IF EXISTS EMPLOYEE")
 #Creating a table
-sql = '''CREATE TABLE EMPLOYEE(
-FIRST_NAME
-LAST_NAME
-CHAR(20) NOT NULL,
-CHAR(20),
-AGE INT, SEX CHAR(1),
-INCOME INT,
-CONTACT INT)'''
+sql = '''
+        CREATE TABLE EMPLOYEE(
+        FIRST_NAME
+        LAST_NAME
+        CHAR(20) NOT NULL,
+        CHAR(20),
+        AGE INT, SEX CHAR(1),
+        INCOME INT,
+        CONTACT INT)
+'''
 cursor.execute(sql)
 #Populating the table
-insert_stmt = "INSERT INTO EMPLOYEE (FIRST_NAME, LAST_NAME, AGE, SEX, INCOME,
-CONTACT) VALUES (%s, %s, %s, %s, %s, %s)"
+insert_stmt = "INSERT INTO EMPLOYEE (FIRST_NAME, LAST_NAME, AGE, SEX, INCOME,CONTACT) VALUES (%s, %s, %s, %s, %s, %s)"
 data = [('Krishna', 'Sharma', 26, 'M', 2000, 101), ('Raj', 'Kandukuri', 20,
 'M', 7000, 102),
 ('Ramya', 'Ramapriya', 29, 'F', 5000, 103),('Mac', 'Mohan', 26, 'M',
